08953456b107070a4064147732978078a9ebe9fe
AxplifyServices <zakariae.zitane@axplify-services.com>
2026-05-06 23:39:25 +0100

    Prepare product import for server

--- database/import/import_products.sql
+++ database/import/import_products.sql
@@ -13,7 +13,7 @@ DELETE FROM products;
 
 ALTER SEQUENCE products_id_seq RESTART WITH 1;
 
-\copy products (name, slug, reference, marque, rubrique, categorie, famille, description, features, specs, url_image1, url_image2, url_image3, url_image4, url_image5, price, sale_price, discount_percent, colors, sizes, stock, weight, badge, is_active, is_featured, is_new, is_bestseller, rating, reviews_count) FROM 'C:/Users/zzaka/OneDrive/Bureau/Projet Dev/Luminaire/database/import/products_import_ready.csv' WITH (FORMAT csv, HEADER true, ENCODING 'UTF8');
+\copy products (name, slug, reference, marque, rubrique, categorie, famille, description, features, specs, url_image1, url_image2, url_image3, url_image4, url_image5, price, sale_price, discount_percent, colors, sizes, stock, weight, badge, is_active, is_featured, is_new, is_bestseller, rating, reviews_count) FROM :csv_path WITH (FORMAT csv, HEADER true, ENCODING 'UTF8');
 
 UPDATE products
 SET updated_at = NOW();
