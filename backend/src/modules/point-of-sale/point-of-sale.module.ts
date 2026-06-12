7f9846d453ab47e2b984d047ef900994f8fbff49
AxplifyServices <zakariae.zitane@axplify-services.com>
2026-06-12 21:16:18 +0100

    Transition

--- backend/src/modules/point-of-sale/point-of-sale.module.ts
+++ backend/src/modules/point-of-sale/point-of-sale.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { PointOfSaleController } from './point-of-sale.controller';
 import { PointOfSaleService } from './point-of-sale.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [PointOfSaleController],
   providers: [PointOfSaleService],
 })
